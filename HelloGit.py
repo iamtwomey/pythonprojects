4e242237f10b4316dc942c027b0ae375d69fcfbf
Mike Twomey <mtwomey3@gmail.com>
2016-12-02 01:25:52 -0500

    edit hello git added new

---
+++ HelloGit.py
@@ -0,0 +1,11 @@
+#test for Git
+print "hello git friend!"
+raw_input = "how are you doing?"
+prompt = ">>"
+import time
+time.sleep(1)
+print "great to see that you're doing well!"
+
+
+time.sleep(3)
+print "oh...one more thing. Brush your teeth before bed.
