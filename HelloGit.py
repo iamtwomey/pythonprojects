0ecd5ed34d7316bb18a73e0d3a54b5a5e744eefe
Mike Twomey <mtwomey3@gmail.com>
2016-12-02 02:08:59 -0500

    added if/else

--- HelloGit.py
+++ HelloGit.py
@@ -4,7 +4,9 @@ doing = raw_input("how are you doing? \n>")
 
 import time
 time.sleep(1)
-print "great to see that you're doing well!"
-
+if doing = "good" or "great"
+    print "Glad to see that you're doing well..."
+else:
+    print "I'm sorry to hear that :( ..."
 time.sleep(3)
 print "oh...one more thing. Brush your teeth before bed."
