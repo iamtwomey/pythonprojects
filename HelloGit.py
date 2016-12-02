8e3ff5382931631b4fbad3782d477acd29cef5a3
Mike Twomey <mtwomey3@gmail.com>
2016-12-02 02:02:17 -0500

    changed line 3

--- HelloGit.py
+++ HelloGit.py
@@ -1,12 +1,10 @@
 #test for Git
 print "hello git friend!"
-doing = raw_input("how are you doing?")
-prompt = ">>"
+doing = raw_input("how are you doing? \n>")
 
 import time
 time.sleep(1)
 print "great to see that you're doing well!"
-
 
 time.sleep(3)
 print "oh...one more thing. Brush your teeth before bed."
