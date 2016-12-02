300b8146ca81fe4c8654f0c6e071fd5974a6cc26
Mike Twomey <mtwomey3@gmail.com>
2016-12-02 01:51:03 -0500

    added space at line 5

--- HelloGit.py
+++ HelloGit.py
@@ -2,10 +2,11 @@
 print "hello git friend!"
 raw_input = "how are you doing?"
 prompt = ">>"
+
 import time
 time.sleep(1)
 print "great to see that you're doing well!"
 
 
 time.sleep(3)
-print "oh...one more thing. Brush your teeth before bed." 
+print "oh...one more thing. Brush your teeth before bed."
